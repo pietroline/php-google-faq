0f59db00f56cf4ae62345805f9d754672001e0f3
pietroline <pietrobarberi@hotmail.it>
2022-03-11 15:51:09 +0100

    style pagina

---
+++ index.php
@@ -0,0 +1,48 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GOOGLE-FAQ</title>
+
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <body>
+
+        <header>
+            
+            <div class="ms_flex ms_justify-content-between">
+
+                <div class="ms_flex ms_align-items-center">
+                    <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="logo Google">
+                    <span class="text">Privacy e termini</span>
+                </div>
+
+                <div class="ms_flex ms_align-items-center">
+                    <i class="fas fa-th ms_grid"></i>
+                    <button>Accedi</button>
+                </div>
+
+            </div>
+
+            <ul>
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li class="active">Domande frequenti</li>
+            </ul>
+        </header>
+
+        <main>
+
+        </main>
+        
+    </body>
+
+</html>
